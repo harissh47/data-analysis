750a9cf99c3d8591b74f06ab0a2f413e750c7d32
harissh47 <jn.harissh@gmail.com>
2025-02-15 11:46:06 +0530

    first push

---
+++ backend/db.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from flask import jsonify
+import os
+load_dotenv()
+client=MongoClient(os.getenv("MONGODB_URI"))
+db=client[os.getenv("DB_NAME")]
+collection=db[os.getenv("COLLECTION_NAME")]
+def connect():
+    data = list(collection.find())
+    return jsonify(data)
