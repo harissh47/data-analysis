984199d77c6a7f049f507788060a70accf129e7e
Aswini M S <aswini.senthilkumar@sifycorp.com>
2025-02-21 12:45:34 +0530

    Implemented Register and Login Screens

---
+++ frontend/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/Login/introduction_page.dart';
+import 'package:frontend/Login/login_page.dart';
+import 'package:frontend/Login/register_page.dart';
+ // Update the path if needed
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: PgIntroductionAnimationScreen(), // Directly loading the LoginPage PgIntroductionAnimationScreen()
+    );
+  }
+}
